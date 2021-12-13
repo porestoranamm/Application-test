54f2657fdea2562330a2c83baafbd2b76b37353d
Nikita Kuznetsov <livisback@yandex.ru>
2021-12-13 20:00:29 +0300

    fix styles code

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './src/app.js',
+    entry: "./src/app.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public"),
     },
     devServer: {
         port: 3000,
-    }
-}
+    },
+};
