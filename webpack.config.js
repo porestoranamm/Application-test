33e6dcbf9e181e7e2396ea909156ca7057f546c1
Nikita Kuznetsov <livisback@yandex.ru>
2021-12-13 19:52:13 +0300

    add script and congfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
-module.exports ={
-    entry: './src/app.js'
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        port: 3000,
+    }
 }
