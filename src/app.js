355ba1c73dd52a156d3d7eb2b47177cf1f85ab23
Nikita Kuznetsov <livisback@yandex.ru>
2021-12-24 08:35:13 +0300

    add request to save question

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import './styles.css'
+import {isValid} from './utils'
 
 const form = document.getElementById('form')
 const input = form.querySelector('#question-input')
@@ -10,5 +11,12 @@ form.addEventListener('submit', submitFormHandler)
 function submitFormHandler(event) {
     event.preventDefault()
 
-    console.log(input.value)
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        // Async request to server to save question
+    }
 }
