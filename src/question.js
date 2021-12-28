dd18774005cee92c7ad0551fe21dfb5b72291406
Nikita Kuznetsov <livisback@yandex.ru>
2021-12-28 11:33:54 +0300

    add function callback

--- src/question.js
+++ src/question.js
@@ -1,5 +1,15 @@
 class Question {
     static create(question) {
-        fetch('https://application-podcast-app-default-rtdb.europe-west1.firebasedatabase.app/')
+        fetch('https://application-podcast-app-default-rtdb.europe-west1.firebasedatabase.app/question.json', {
+            method: 'POST',
+            body: JSON.stringify(question),
+            headers: {
+                'Conten-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+        })
     }
 }
