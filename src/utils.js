0be70908cbd27a8ddb9695dcf275e6b8d88e010d
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-02 17:53:45 +0300

    add function  or create modal window

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
+import { MultiCompiler } from "webpack"
+
 export function isValid(value) {
     return value.length >= 10
+}
+
+
+export function createModal(title, content) {
+    const modal = document.createElement('div')
+    modal.classList.add('modal')
+
+    mui.overlay('on', modal)
 }
