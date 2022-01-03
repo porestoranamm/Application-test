8d336241b418d4cf20cefc5fe4d935b179e0b4f6
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-03 17:45:43 +0300

    add modal window content

--- src/utils.js
+++ src/utils.js
@@ -9,5 +9,10 @@ export function createModal(title, content) {
     const modal = document.createElement('div')
     modal.classList.add('modal')
 
+    modal.innerHTML = `
+    <h1>${title}</h1>
+    <div class="modal-content">${content}</div>
+    `
+
     mui.overlay('on', modal)
 }
