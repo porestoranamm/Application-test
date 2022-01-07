4bd838edb5da1764abf1bc476f9c14fc79049ffe
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-07 15:10:10 +0300

    create function authorization

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,15 @@
 export function getAuthForm() {
- return `
-    <form class="mui-form" id="auth-form">
-    <div class="mui-textfield mui-textfield--float-label">
-    <input type="email" id="email" required>
-    <label for="email" disabled>Email</label>
-    </div>
-    button type="submit" class="mui-btn mui-btn--raised mui-btn--primary">Задать вопрос</button>
-    </form>
- `
+   return `
+      <form class="mui-form" id="auth-form">
+      <div class="mui-textfield mui-textfield--float-label">
+      <input type="email" id="email" required>
+      <label for="email" disabled>Email</label>
+      </div>
+      <div class="mui-textfield mui-textfield--float-label">
+      <input type="password" id="password" required>
+      <label for="password" disabled>Пароль</label>
+      </div>
+      <button type="submit" class="mui-btn mui-btn--raised mui-btn--primary">Войти</button>
+      </form>
+   `
 }
