df9ae491f7e3617d94ea8a2bf06bafe2c744c622
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-07 15:42:46 +0300

    logic authorization (firebase apiKey)

--- src/auth.js
+++ src/auth.js
@@ -13,3 +13,20 @@ export function getAuthForm() {
       </form>
    `
 }
+
+export function authWithEmailAndPassword(email, password) {
+   const apiKey = 'AIzaSyAJXqAtoI765PHKqs2V2G39W80J9ftCRPo'
+   return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}`, {
+      method: 'POST',
+      body: JSON.stringify({
+         email, password,
+         returnSecureToken: true
+      }),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   })
+   .then(response => response,json())
+   .then(data => console.log(data))
+
+}
